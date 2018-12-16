86e775cf84342b604656aac8f75c1dff8f25c0bd
Marek Sybilak <marek.sybilak@gmail.com>
2018-12-16 21:26:55 +0100

    cleanup

--- panel/admin_panel.py
+++
@@ -1,8 +0,0 @@
-from sanic.response import json
-from sanic import Blueprint
-
-panel = Blueprint(name='admin_panel')
-
-@panel.route('/')
-async def index(request):
-    return json({'my': 'panel użytkownika - administratora'})
